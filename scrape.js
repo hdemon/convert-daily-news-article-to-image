1759f97a9dbe66fdbe83728593771831619df97d
masami.yonehara <masami.yonehara@linecorp.com>
2018-01-10 12:46:20 +0900

    Remove font size box

--- scrape.js
+++ scrape.js
@@ -2,19 +2,21 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://www.rarejob.com/dna/');
   await page.screenshot({ path: 'example.png' });
   await page.click('main > article > header > h1 > a')
-  // await page.waitForSelector('body[class~="single-post"]')
-  await page.waitForSelector('#fontL')
+  await page.waitForSelector('body[class~="single-post"]')
   await page.evaluate(() => {
     const sidebar = document.querySelector('#sidebar1')
     const footer = document.querySelector('.footer')
     const header = document.querySelector('#container')
+    const font_size_box = document.querySelector('.fsz_box')
     sidebar.parentNode.removeChild(sidebar)
     footer.parentNode.removeChild(footer)
     header.parentNode.removeChild(header)
+    font_size_box.parentNode.removeChild(font_size_box)
   })
 
   await page.screenshot({ path: 'example.png',  fullPage: true });
